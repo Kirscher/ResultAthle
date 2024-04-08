10604528a3eb57c6fd97a435b17199581dd49929
tamadei <tristan.amadei@ensae.fr>
2024-04-08 17:06:27 +0000

    adding docs

---
+++ app/docs/index.rst
@@ -0,0 +1,20 @@
+.. ResultAthle documentation master file, created by
+   sphinx-quickstart on Mon Apr  8 12:59:22 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to ResultAthle's documentation!
+=======================================
+
+.. toctree::
+   :maxdepth: 4
+   :caption: Contents:
+
+   modules
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- app/visu.py
+++ app/visu.py
@@ -1,45 +1,72 @@
-import pandas as pd
 import matplotlib.pyplot as plt
 
-figsize = (8, 4)
-color = "darkslateblue"
+FIGSIZE = (8, 4)
+COLOR = "darkslateblue"
 
 
 def plot_distribution(df, column):
     """
-    Plot a histogram for a specific column in the dataframe
+    Plot a histogram for a specific column in the dataframe.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The DataFrame containing the data.
+    column : str
+        The name of the column to plot.
     """
-    plt.figure(figsize=figsize)
-    df[column].hist(bins=30, color=color)
+
+    plt.figure(figsize=FIGSIZE)
+    df[column].hist(bins=30, color=COLOR)
     plt.title(f"Distribution of {column}")
     plt.show()
 
 
 def plot_category_distribution(df, column):
     """
-    Plot a bar chart for a categorical column in the dataframe
+    Plot a bar chart for a categorical column in the dataframe.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The DataFrame containing the data.
+    column : str
+        The name of the categorical column to plot.
     """
-    plt.figure(figsize=figsize)
-    df[column].value_counts().plot(kind="bar", color=color)
+
+    plt.figure(figsize=FIGSIZE)
+    df[column].value_counts().plot(kind="bar", color=COLOR)
     plt.title(f"Distribution of {column}")
     plt.show()
 
 
 def plot_time_gap(df):
     """
-    Plot a line chart for time gap
+    Plot a line chart for time gap.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The DataFrame containing the data.
     """
-    plt.figure(figsize=figsize)
-    df["time_gap"].value_counts().sort_index().plot(color=color)
+
+    plt.figure(figsize=FIGSIZE)
+    df["time_gap"].value_counts().sort_index().plot(color=COLOR)
     plt.title("Time Gap")
     plt.show()
 
 
 def plot_duration(df):
     """
-    Plot a line chart for duration
+    Plot a line chart for duration.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The DataFrame containing the data.
     """
-    plt.figure(figsize=figsize)
-    df["duration"].plot(color=color)
+
+    plt.figure(figsize=FIGSIZE)
+    df["duration"].plot(color=COLOR)
     plt.title("Duration")
     plt.show()
