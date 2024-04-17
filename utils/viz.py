921563cc4b6ac9fbd64bd62863abb0c73698828c
Tristan Kirscher <85068746+Kirscher@users.noreply.github.com>
2024-04-17 07:46:02 +0000

    [+] added viz library

---
+++ utils/viz.py
@@ -0,0 +1,40 @@
+import plotly.graph_objects as go
+import numpy as np
+import pandas as pd
+import utils.stat as stat
+
+def graphePerso(prenom, nom, data, titre):
+    densite=stat.dens(data['duration'], bins = stat.idealBins(len(data['duration'])))
+    fcubic=stat.lissage(densite, sep = True,beginend = (data['duration'].min(),data['duration'].max()))
+    z=data.index[data.Athlète==(nom.upper()+" "+prenom.capitalize())]
+    temps = data.loc[z[0],'duration']
+    x= np.linspace(data['duration'].min(),data['duration'].max(), int(1e5))
+
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(
+        x=x, y=fcubic(x),
+        fill='tozeroy',
+        mode='lines', 
+        line_color='blue',
+        name='densité lissée & interpolée'
+    ))
+
+    fig.add_trace(go.Scatter(
+        x=[temps, temps], y=[0, max(fcubic(x))],
+        mode='lines',
+        line_color='red',
+        name=nom.upper()+" "+prenom.capitalize()
+    ))
+
+    fig.update_layout(
+        title=titre,
+        xaxis_title="Durée pour franchir la ligne d'arrivée",
+        yaxis_title="Densité des athlètes",
+        legend_title="Légende",
+        autosize=False,
+        width=800,
+        height=450,
+    )
+
+    fig.show()
