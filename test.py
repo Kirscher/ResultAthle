c13970b8d6e991906c3503345a303b991c9b3d0b
Tristan Kirscher <85068746+Kirscher@users.noreply.github.com>
2024-04-17 08:10:30 +0000

    [*] fixed test

--- test.py
+++ test.py
@@ -5,6 +5,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-data = scraping.get_results("https://bases.athle.fr/asp.net/liste.aspx?frmbase=resultats&frmmode=1&frmespace=0&frmcompetition=282742", 13)
+header, data = scraping.get_results("https://bases.athle.fr/asp.net/liste.aspx?frmbase=resultats&frmmode=1&frmespace=0&frmcompetition=282742", 13)
 
 print(data.head())
